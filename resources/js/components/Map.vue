7a9ea59b1c1b4532542382717555d9fdb4b7303c
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2022-04-01 15:46:39 +0200

    validation search

--- resources/js/components/Map.vue
+++ resources/js/components/Map.vue
@@ -1,75 +1,107 @@
 <template>
-  <div>
-    <!-- search bar  -->
-    <div class="input-group my-5 justify-content-center">
-        <div class="form-outline">
-            <input type="search" id="form1" class="form-control" placeholder="Search" v-model="query"/>
-        </div>
-        <button type="button" class="btn btn-primary" @click="search()">
-            <i class="fas fa-search"></i>
-        </button>
+    <div>
+        <!-- search bar  -->
+            <div class="input-group my-5 justify-content-center">
+                <div class="form-outline">
+                    <input
+                        type="search"
+                        id="searchBar"
+                        class="form-control"
+                        placeholder="Search"
+                        v-model="query"
+                        @keyup.enter="search()"
+                        required
+                    />
+                    <p id="searchDemo"></p>
+                </div>
+                <button
+                    type="button"
+                    class="btn btn-primary"
+                    @click="search()" 
+					value="Submit form"
+                >
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+        <div class="map" id="map" ref="mapRef"></div>
     </div>
-    <!-- <input type="text" id="query" value="" v-model="query">
-    <button @click="search()">Search</button> -->
-    <div class="map" id="map" ref="mapRef"></div>
-  </div>
 </template>
 
 <script>
-import tt from '@tomtom-international/web-sdk-maps';
+import tt from "@tomtom-international/web-sdk-maps";
 import tts from "@tomtom-international/web-sdk-services";
 export default {
-  name: 'Map',
-  data() {
-    return {
-      map: null,
-      query: "",
-      position: {
-        lng: 12.4829321,
-        lat: 41.8933203,
-      }
-      
-    }
-  },
-  
-  mounted() {
-    this.initializeMap(this.position.lng, this.position.lat);
-  },
-  methods: {
-    initializeMap(lng, lat) { 
-    this.map = tt.map({
-    key: '2PavVFdEzd44ElVnixCMPjU42Wgfsj6Z',
-    container: this.$refs.mapRef,
-    center: [lng, lat],
-    zoom: 12
-    });
-    new tt.Marker()
-      .setLngLat([lng, lat])
-      .addTo(this.map);
-    this.map.addControl(new tt.FullscreenControl());
-    this.map.addControl(new tt.NavigationControl());
+    name: "Map",
+    data() {
+        return {
+            map: null,
+            query: null,
+            position: {
+                lng: 12.4829321,
+                lat: 41.8933203,
+            },
+        };
     },
-    moveMap: function(lnglat) {
-      this.map.flyTo({
-        center: lnglat,
-        zoom: 12,
-      })
+
+    mounted() {
+        this.initializeMap(this.position.lng, this.position.lat);
     },
-    handleResults: function(result) {
-      if (result.results) {
-        this.moveMap(result.results[0].position)
-      }
-      this.position.lng = result.results[0].position.lng;
-      this.position.lat = result.results[0].position.lat;
+    methods: {
+        validateSearch: function () {
+            if (this.query && isNaN(this.query)) {
+				return true;
+            } else {
+				return false;
+            }
+        },
+        initializeMap(lng, lat) {
+            this.map = tt.map({
+                key: "2PavVFdEzd44ElVnixCMPjU42Wgfsj6Z",
+                container: this.$refs.mapRef,
+                center: [lng, lat],
+                zoom: 12,
+            });
+            new tt.Marker().setLngLat([lng, lat]).addTo(this.map);
+            this.map.addControl(new tt.FullscreenControl());
+            this.map.addControl(new tt.NavigationControl());
+        },
+        moveMap: function (lnglat) {
+            this.map.flyTo({
+                center: lnglat,
+                zoom: 12,
+            });
+        },
+        handleResults: function (result) {
+            if (result.results) {
+                this.moveMap(result.results[0].position);
+            }
+            this.position.lng = result.results[0].position.lng;
+            this.position.lat = result.results[0].position.lat;
+            this.query = "";
+        },
+        search: function () {
+            let error = document.getElementById("searchDemo");
+            let message = "";
+			if (this.validateSearch()) {
+				error.innerHTML = "";
+                error.classList.remove("alert");
+                error.classList.remove("alert-danger");
+				tts.services
+					.fuzzySearch({
+						key: "2PavVFdEzd44ElVnixCMPjU42Wgfsj6Z",
+						query: this.query,
+					})
+					.then(this.handleResults);
+			} else {
+				this.query = null;
+				message = "adress not valid";
+                error.innerHTML = message;
+                error.classList.add("alert");
+                error.classList.add("alert-danger");
+			}
+        },
     },
-    search: function() {
-      tts.services.fuzzySearch({
-        key: '2PavVFdEzd44ElVnixCMPjU42Wgfsj6Z',
-        query: this.query,
-      }).then(this.handleResults)
-    }
-  }
-}
+};
 </script>
 
 <style lang="scss" scoped>
