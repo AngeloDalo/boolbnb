e606d2815a2eef1e6240d5edc309c7ff85051519
AngeloDalo <angelo251158@gmail.com>
2022-04-05 21:09:55 +0200

    prova mappa

--- resources/js/pages/Apartment.vue
+++ resources/js/pages/Apartment.vue
@@ -1,26 +1,26 @@
 <template>
-<div class="container mt-5">
-    <div class="row border border-danger rounded-3 p-3">
-        <div class="col-6">
-            <img class="w-100 rounded-3" :src="'http://127.0.0.1:8000/storage/' + apartment.image" :alt="apartment.title">
+    <div class="container">
+        <div class="row border border-danger rounded-3 p-3">
+            <div class="col-6">
+                <img class="w-100 rounded-3" :src="'http://127.0.0.1:8000/storage/' + apartment.image" :alt="apartment.title">
+            </div>
+            <div class="col-6">
+                <h3 class="font-weight-bold text-danger">{{ apartment.title }}</h3>
+                <p>Prirce: {{ apartment.price }}</p>
+                <p>Rooms: {{ apartment.rooms }}</p>
+                <p>Bed: {{ apartment.beds }}</p>
+                <p>Square: {{ apartment.square }}</p>
+                <p>Address: {{ apartment.address }}</p>
+                <router-link class="btn btn-danger mb-2 text-white" :to="{ name: 'contact', params: { id: apartment.id }}"> <span class="routerApartment d-none">{{ apartment.id }}</span>Contact Owner</router-link>
+            </div>
         </div>
-        <div class="col-6">
-            <h3 class="font-weight-bold text-danger">{{ apartment.title }}</h3>
-            <p>Prirce: {{ apartment.price }}</p>
-            <p>Rooms: {{ apartment.rooms }}</p>
-            <p>Bed: {{ apartment.beds }}</p>
-            <p>Square: {{ apartment.square }}</p>
-            <p>Address: {{ apartment.address }}</p>
-            <router-link class="btn btn-danger mb-2 text-white" :to="{ name: 'contact', params: { id: apartment.id }}"> <span class="routerApartment d-none">{{ apartment.id }}</span>Contact Owner</router-link>
-        </div>
+        <!-- mappa -->
+        <div class="map" id="map" ref="mapRef"></div>
     </div>
-    <div class="map" id="map" ref="mapRef"></div>
-</div>
 </template>
 
 <script>
-import tt from "@tomtom-international/web-sdk-maps";
-import tts from "@tomtom-international/web-sdk-services";
+import tt from '@tomtom-international/web-sdk-maps';
 import Axios from "axios";
   export default {
     name: 'Apartment',
@@ -36,7 +36,7 @@ import Axios from "axios";
         this.getApartment(url);
     },
     mounted() {
-        this.initializeMap(17.277969, 40.4657082);
+        this.initializeMap()
     },
     methods: {
         getApartment(url){
@@ -45,23 +45,37 @@ import Axios from "axios";
                 this.apartment = result.data.results.data;
             }).catch(error => console.log(error));
         },
-        initializeMap(lng, lat) {
+        initializeMap() {
             this.map = tt.map({
-                key: "2PavVFdEzd44ElVnixCMPjU42Wgfsj6Z",
-                container: this.$refs.mapRef,
-                center: [lng, lat],
-                zoom: 9,
-            });
+            key: '2PavVFdEzd44ElVnixCMPjU42Wgfsj6Z',
+            container: this.$refs.mapRef,
+            center: [17.277969, 40.4657082],
+            zoom: 14
+        });
+        new tt.Marker()
+            .setLngLat([17.277969, 40.4657082])
+            .addTo(this.map);
             this.map.addControl(new tt.FullscreenControl());
             this.map.addControl(new tt.NavigationControl());
-        },
+        }
+  
+        // initializeMap(lng, lat) {
+        //     this.map = tt.map({
+        //         key: "2PavVFdEzd44ElVnixCMPjU42Wgfsj6Z",
+        //         container: this.$refs.mapRef,
+        //         center: [lng, lat],
+        //         zoom: 9,
+        //     });
+        //     this.map.addControl(new tt.FullscreenControl());
+        //     this.map.addControl(new tt.NavigationControl());
+        // },
     }
   }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #map {
-    width: 45%;
-    height: 40vh;
+    height: 400px;
+    width: 100%;
 }
 </style>
