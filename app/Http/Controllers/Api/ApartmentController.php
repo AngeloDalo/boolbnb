470bd7a754afdd1ecac6382da993f8313992e716
Guz117 <cristian.guzman117@gmail.com>
2022-04-06 17:48:37 +0200

    testing error

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -24,25 +24,46 @@ class ApartmentController extends Controller
     {
         $data = $request->all();
 
+        $KmRaggio = $data['km'];
+        $rooms = $data['rooms'];
+        $beds = $data['beds'];
+        $position = $data['position'];
+        $searchedLat = $position['lat'];
+        $searchedLng = $position['lng'];
+        $apartmentDistance = [];
+        $checkedSer = $data['checkedServices'];
+
+
+
         //aperta chiamata ma non chiusa per avere continui aggiornamenti
         $apartments = Apartment::where('id', '>=', 1);
 
-        //Se esistono i services all'interno di data passati tramite request
-        if (array_key_exists('services', $data)) {
-            foreach ($data['services'] as $service) {
-                $apartments->whereHas('services', function (Builder $query) use ($service) {
-                    $query->where('name', '=', $service);
-                });
+
+        $apartments = $apartments->with('services')->where('visible', '=', 1)->where('rooms', '>=', $rooms)->where('beds', '>=', $beds);
+        foreach ($data['checkedServices'] as $service) {
+            $apartments = $apartments->whereHas('services', function (Builder $query) use ($service) {
+                $query->where('name', '=', $service);
+            });
+        }
+        $apartments = $apartments->get();
+
+        foreach ($apartments as $key => $apartment) {
+            $apartmentLat = floatval($apartment->latitude);
+            $apartmentLng = floatval($apartment->longitude);
+            $distance = distance($searchedLat, $searchedLng, $apartmentLat, $apartmentLng);
+            if ($distance > $KmRaggio) {
+                $apartments = $apartments->except($apartment->id);
+            } else {
+                array_push($apartmentDistance, $distance);
             }
         }
 
-        $apartments = $apartments->with(['services'])->get();
-
         return response()->json([
             'response' => true,
-            'count' =>  $apartments->count(),
             'results' =>  [
-                'data' => $apartments
+                'apartments' => $apartments,
+                'data' => $data,
+                'checkedser' => $checkedSer
             ],
         ]);
     }
@@ -57,4 +78,20 @@ class ApartmentController extends Controller
             ],
         ]);
     }
+}
+
+function distance($lat1, $lon1, $lat2, $lon2)
+{
+    $r = 6372.797; // mean radius of Earth in km
+    $latFrom = deg2rad($lat1);
+    $lonFrom = deg2rad($lon1);
+    $latTo = deg2rad($lat2);
+    $lonTo = deg2rad($lon2);
+
+    $latDelta = $latTo - $latFrom;
+    $lonDelta = $lonTo - $lonFrom;
+
+    $angle = 2 * asin(sqrt(pow(sin($latDelta / 2), 2) +
+        cos($latFrom) * cos($latTo) * pow(sin($lonDelta / 2), 2)));
+    return $angle * $r;
 }
