22c0e292e7e587d340ce02a04b902ae867d12301
AngeloDalo <angelo251158@gmail.com>
2022-04-07 18:48:54 +0200

    home

--- resources/js/pages/Sponsor.vue
+++ resources/js/pages/Sponsor.vue
@@ -5,128 +5,36 @@
         </div>
 
         <div class="container-fluid py-4">
-            <div class="row">
-                <div class="" v-if="apartments.length == 0">
-                    <div
-                        class="card mb-5 border border-danger col-lg-4"
-                        v-for="apartment in apartments"
-                        :key="apartment.id"
-                    >
-                        <div class="div-img">
-                            <img
-                                class="card-img-top p-1"
-                                :src="'storage/' + apartment.image"
-                                :alt="apartment.title"
-                            />
-                            <p class="price text-light px-3">
-                                {{ apartment.price }} &euro;
-                            </p>
-                        </div>
-
-                        <div class="card-body">
-                            <h3
-                                class="font-weight-bold text-danger font-weight-bold"
-                            >
-                                {{ apartment.title }}
-                            </h3>
-
-                            <p class="p-sponsor">
-                                <span class="span-sponsor">Address:</span>
-                                {{ apartment.address }}
-                            </p>
-                            <span class="p-sponsor">
-                                <span class="span-sponsor">Rooms:</span>
-                                {{ apartment.rooms }}
-                            </span>
-                            <span class="p-sponsor">
-                                <span class="span-sponsor">Beds:</span>
-                                {{ apartment.rooms }}
-                            </span>
-                            <span class="p-sponsor"
-                                >Beds: {{ apartment.beds }}</span
-                            >
-                            <span class="p-sponsor">
-                                <span class="span-sponsor">Bathrooms:</span>
-                                {{ apartment.bathrooms }}
-                            </span>
-                            <span class="p-sponsor">
-                                <span class="span-sponsor">Square:</span>
-                                {{ apartment.square }}
-                            </span>
-                            <div>
-                                <router-link
-                                    class="btn btn-danger mb-2"
-                                    :to="{
-                                        name: 'apartment',
-                                        params: { id: apartment.id },
-                                    }"
-                                >
-                                    View
-                                </router-link>
+                <div class="container row" v-if="apartments.length != 0">
+                        <div class="div col-4 me-2 border-danger border" v-for="apartment in apartments" :key="apartment.id">
+                            <div class="div-img">
+                                <img class="card-img-top p-1" :src="'storage/' + apartment.image" :alt="apartment.title"/>
+                                <p class="price text-light px-3">{{ apartment.price }} &euro;</p>
                             </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="" v-else>
-                    <div
-                        class="card mb-5 border border-danger col-lg-4"
-                        v-for="apartment in apartments"
-                        :key="apartment.id"
-                    >
-                        <div class="div-img">
-                            <img
-                                class="card-img-top p-1"
-                                :src="'storage/' + apartment.image"
-                                :alt="apartment.title"
-                            />
-                            <p class="price text-light px-3">
-                                {{ apartment.price }} &euro;
-                            </p>
-                        </div>
 
-                        <div class="card-body">
-                            <h3
-                                class="font-weight-bold text-danger font-weight-bold"
-                            >
-                                {{ apartment.title }}
-                            </h3>
-                            <!-- <p class="p-sponsor">
-                            <span class="span-sponsor">Price:</span>
-                            {{ apartment.price }} &euro;
-                        </p> -->
-                            <p class="p-sponsor">{{ apartment.address }},</p>
-                            <span class="p-sponsor">
-                                <span class="span-sponsor">Rooms:</span>
-                                {{ apartment.rooms }},
-                            </span>
-                            <span class="p-sponsor">
-                                <span class="span-sponsor">Beds:</span>
-                                {{ apartment.beds }},
-                            </span>
-                            <span class="p-sponsor">
-                                <span class="span-sponsor">Bathrooms:</span>
-                                {{ apartment.bathrooms }},
-                            </span>
-                            <span class="p-sponsor">
-                                <span class="span-sponsor">Square:</span>
-                                {{ apartment.square }} m<sup>3</sup>
-                            </span>
-                            <div>
-                                <router-link
-                                    class="btn btn-danger my-2"
-                                    :to="{
-                                        name: 'apartment',
-                                        params: { id: apartment.id },
-                                    }"
-                                >
-                                    View for more details
-                                </router-link>
+                            <div class="card-body">
+                                <h3 class="font-weight-bold text-danger font-weight-bold">{{ apartment.title }}</h3>
+                                <p class="p-sponsor"><span class="span-sponsor">Address:</span>{{ apartment.address }}</p>
+                                <span class="p-sponsor"><span class="span-sponsor">Rooms:</span>{{ apartment.rooms }}</span>
+                                <span class="p-sponsor"><span class="span-sponsor">Beds:</span>{{ apartment.rooms }}</span>
+                                <span class="p-sponsor">Beds: {{ apartment.beds }}</span>
+                                <span class="p-sponsor"><span class="span-sponsor">Bathrooms:</span>{{ apartment.bathrooms }}</span>
+                                <span class="p-sponsor"><span class="span-sponsor">Square:</span>{{ apartment.square }}</span>
+                                <div>
+                                    <router-link
+                                        class="btn btn-danger mb-2"
+                                        :to="{
+                                            name: 'apartment',
+                                            params: { id: apartment.id },
+                                        }"
+                                    >
+                                        View
+                                    </router-link>
+                                </div>
                             </div>
-                        </div>
                     </div>
                 </div>
             </div>
-        </div>
     </div>
 </template>
 
