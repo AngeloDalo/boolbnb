ec65f6bc7e4b4f6dc87b6d58eb0c7d508ab6b005
AngeloDalo <angelo251158@gmail.com>
2022-04-09 15:05:51 +0200

    push

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -2,15 +2,18 @@
     <div>
         <Header></Header>
         <router-view></router-view>
+        <!-- <Footer></Footer> -->
     </div>
 </template>
 
 <script>
 import Header from "../components/Header.vue";
+// import Footer from "../components/Footer.vue";
 export default {
     name: "App",
     components: {
         Header,
+        // Footer,
     },
 };
 </script>
